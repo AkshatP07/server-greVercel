110cf9cd5732620e870f5b350ae3e4dfdb224177
akshat <mejatin03gmail.com>
2024-10-06 19:18:35 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'https://client-gre-vercel-qokg.vercel.app'
 }));
 app.use(bodyParser.json());
 
