5d50f87b5738a6a2bf0f50cfb3aec0d822449350
akshat <mejatin03gmail.com>
2024-10-06 15:43:47 +0530

    vercel ready

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,28 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
     dialect: 'postgres',
     logging: false, // To disable SQL logging, helps during development
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // If you're using a self-signed certificate
+        }
+    }
 });
 
 module.exports = sequelize;
+
+
+//old files
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: false, // To disable SQL logging, helps during development
+// });
+
+// module.exports = sequelize;
